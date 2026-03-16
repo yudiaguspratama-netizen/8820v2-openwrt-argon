5dfbf001c8ded9065656097d2ed3dd12af07b0eb
yudiaguspratama-netizen <yudiaguspratama@gmail.com>
2026-03-16 17:47:44 +0700

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
+# Hapus feeds lama dan ambil yang segar
+rm -rf feeds
+./scripts/feeds update -a
+./scripts/feeds install -a
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# Tambahkan library yang sering hilang pada build mipsel
+./scripts/feeds install libncurses5-dev
